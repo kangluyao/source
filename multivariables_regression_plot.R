c6feba364e45c529b25ecd5d90e976a8fe54232d
kangluyao <luyaokang@outlook.com>
2023-04-20 22:33:37 +0800

    Update multivariables_regression_plot.R

--- multivariables_regression_plot.R
+++ multivariables_regression_plot.R
@@ -1,10 +1,61 @@
+# correlation plot
+plot_cor_df <- function(
+    data = NULL,
+    y.name = NULL,
+    x.names = NULL,
+    ncol = NULL,
+    method = "pearson",
+    point.color = "tomato3",
+    line.color = "black"
+){
+  if(
+    is.null(data) |
+    is.null(y.name) |
+    is.null(x.names)
+  ){
+    stop("No variables to plot.")
+  }
+  if (method == "pearson"){
+    labels = c("cor", "p.value")
+  } else if (method == "spearman"){
+    labels = c("rho", "p.value")
+  } else if (method == "kendall"){
+    labels = c("tau", "p.value")
+  }
+  #x.names comes from environmental variables
+  if(!is.null(x.names)){
+    if(inherits(x.names, "variable_selection")){
+      x.names <- x.names$selected.variables
+    }
+  }
+  plot.list <- list()
+  for(variable in x.names){
+    plot.list[[variable]] <- ggplot2::ggplot(
+      data = data,
+      ggplot2::aes_string(
+        x = variable,
+        y = y.name,
+        color = y.name
+      )
+    ) +
+      ggplot2::geom_point(colour = point.color) +
+      ggplot2::theme_bw() +
+      ggplot2::theme(legend.position = "none") +
+      ggpmisc::stat_poly_line(formula = y ~ x, color = line.color) +
+      ggpmisc::stat_correlation(method = method, 
+                                mapping = ggpmisc::use_label(labels))
+  }
+  p <- patchwork::wrap_plots(plot.list, ncol = ncol)
+  p
+}
+
 # linear and loess regression plot
 plot_loess_df <- function(
     data = NULL,
     y.name = NULL,
     x.names = NULL,
     ncol = NULL,
-    method = "loess",
+    line.method = "loess",
     point.color = "tomato3",
     line.color = "black"
 ){
@@ -36,24 +87,27 @@ plot_loess_df <- function(
       ggplot2::theme_bw() +
       ggplot2::theme(legend.position = "none") +
       ggplot2::geom_smooth(
-        method = method,
+        method = line.method,
         col = line.color,
         formula = y ~ x,
         se = TRUE,
         alpha = 0.75
-      )
+      ) +
+      ggpubr::stat_cor(aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+                       p.accuracy = 0.001, r.accuracy = 0.01,
+                       method = "pearson", label.x.npc = 0.3, label.y.npc = 0.05, size = 4)
   }
   p <- patchwork::wrap_plots(plot.list, ncol = ncol)
   p
 }
 
-# quadratic regression plot
-plot_quadr_df <- function(
+# poly regression plot
+plot_poly_df <- function(
     data = NULL,
     y.name = NULL,
     x.names = NULL,
+    poly = 2,
     ncol = NULL,
-    method = "loess",
     point.color = "tomato3",
     line.color = "black"
 ){
@@ -84,13 +138,12 @@ plot_quadr_df <- function(
       ggplot2::geom_point(colour = point.color) +
       ggplot2::theme_bw() +
       ggplot2::theme(legend.position = "none") +
-      ggplot2::geom_smooth(
-        method = method,
-        col = line.color,
-        formula = y ~ poly(x, 2),
-        se = TRUE,
-        alpha = 0.75
-      )
+      ggpmisc::stat_poly_line(formula = y ~ poly(x, poly), color = line.color) +
+      ggpmisc::stat_poly_eq(aes(label =  paste(after_stat(eq.label), "*\", \"*",
+                                  after_stat(rr.label), "*\", \"*", 
+                                  after_stat(p.value.label), "*\".\"",
+                                  sep = "")),
+    formula = y ~ poly(x, poly), size = 3)
   }
   p <- patchwork::wrap_plots(plot.list, ncol = ncol)
   p
@@ -140,6 +193,12 @@ plot_gam_df <- function(
         se = TRUE,
         alpha = 0.75
       )
+      # ggpmisc::stat_poly_line(formula = y ~ splines::ns(x, 2), color = line.color) +
+      # ggpmisc::stat_poly_eq(aes(label =  paste(after_stat(eq.label), "*\", \"*",
+      #                                          after_stat(rr.label), "*\", \"*", 
+      #                                          after_stat(p.value.label), "*\".\"",
+      #                                          sep = "")),
+      #                       formula = y ~ splines::ns(x, 2), size = 3)
   }
   p <- patchwork::wrap_plots(plot.list, ncol = ncol)
   p
